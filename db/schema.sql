951d68c06ac95c0d6097f59176d0c7d0bb2a5b95
unknown <twicklvnv@yahoo.com>
2018-02-19 19:02:48 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(200) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Green Chile Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("BBQ Cheeseburger");
